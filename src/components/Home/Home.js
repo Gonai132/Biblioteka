acd24e4ffb803e7e23d5a0cb1227ed9dfbae4eed
Gonai <margaret.szymanik@gmail.com>
2025-04-07 18:25:07 +0200

    creating app structure and navbar

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import './home.css';
+
+function Home() {
+    return (
+    <div className="home">
+    <h1>Strona główna</h1>
+    <h1>Strona główna</h1>
+    <h1>Strona główna</h1>
+    <h1>Strona główna</h1>
+    <h1>Strona główna</h1>
+    </div>
+    )
+}
+export default Home
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom"; // Zaimportuj Link z react-router-dom
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import "./navbar.css";
+
+const Navbar = () => {
+  const [navScroll, setNavScroll] = useState(false);
+
+  const onChangeNav = () => {
+    if (window.scrollY >= 50) {
+      setNavScroll(true);
+    } else {
+      setNavScroll(false);
+    }
+  };
+
+  window.addEventListener("scroll", onChangeNav);
+
+  return (
+    <nav className={navScroll ? "navbar-main navbar-scroll" : "navbar-main"}>
+      <ul>
+        <li className="nav-item home-nav">
+          <Link to="/"> {/* Zamień FontAwesomeIcon na Link */}
+            <FontAwesomeIcon icon={faHome} />
+          </Link>
+        </li>
+        <li className="nav-item gap"> </li>
+        <li className="nav-item">
+          <Link to="/about">O nas</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/books">Książki</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/rent">Wypożycz</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/gallery">Zwróć</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact">Kontakt</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
